413ae3e23cf52a139f1ee4dd8c6333cd83f04bb4
default <marco.cantusci@stu.ocsb.ca>
2025-11-10 09:50:47 +0000

    Finished A1

--- A1.ts
+++ A1.ts
@@ -6,18 +6,18 @@
  */
 
 // store radius as 5.6
-let radius: number = 5.6
+const RADIUS: number = 5.6;
 
 // store pi as 3.14
-const PI: number = 3.14
+const PI: number = 3.14;
 
 // formula/calculation of area
-let formula: number = PI * radius * radius
+let formula: number = PI * RADIUS * RADIUS;
 
 // print radius
-console.log("Radius: " + radius + "cm")
+console.log("Radius: " + RADIUS + "cm");
 
 // print area
-console.log("The area of a circle with a radius of " + radius + " cm is " + formula + " cm²")
+console.log("The area of a circle with a radius of " + RADIUS + " cm is " + formula + " cm²");
 
-console.log("\nDone.")
+console.log("\nDone.");
