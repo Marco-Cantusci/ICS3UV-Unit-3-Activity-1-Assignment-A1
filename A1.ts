34db1f3947617f7ccfbe5dfdf4b9da055cfd4d8b
default <marco.cantusci@stu.ocsb.ca>
2025-11-10 09:42:01 +0000

    Finished A1

---
+++ A1.ts
@@ -0,0 +1,23 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-11-10
+ * @fileoverview Display and calculate the area of a circle with a radius of 5.6 cm.
+ */
+
+// store radius as 5.6
+let radius: number = 5.6
+
+// store pi as 3.14
+const PI: number = 3.14
+
+// formula/calculation of area
+let formula: number = PI * radius * radius
+
+// print radius
+console.log("Radius: " + radius + "cm")
+
+// print area
+console.log("The area of a circle with a radius of " + radius + " cm is " + formula + " cm²")
+
+console.log("\nDone.")
